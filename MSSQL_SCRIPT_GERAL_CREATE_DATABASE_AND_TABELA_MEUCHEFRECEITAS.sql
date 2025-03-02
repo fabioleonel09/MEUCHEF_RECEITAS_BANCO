019412530689ebc6d13a09a481bd0122c43c177f
Fábio Leonel <92651510+fabioleonel09@users.noreply.github.com>
2025-03-01 22:32:43 -0300

    MEUCHEF: Banco | Script de criação da DB e tabela.

---
+++ MSSQL_SCRIPT_GERAL_CREATE_DATABASE_AND_TABELA_MEUCHEFRECEITAS.sql
@@ -0,0 +1,27 @@
+--SCRIPT GERAL DE CRIAÇÃO DA BASE DE DADOS E DE TODAS AS TABELAS DO MEUCHEF RECEITAS
+--Criação da data base
+IF NOT EXISTS (SELECT 1 FROM sys.databases WHERE name = 'MEUCHEFRECEITAS')
+
+BEGIN
+	CREATE DATABASE MEUCHEFRECEITAS;
+END
+	
+GO
+
+--Usando a data base criada	
+USE MEUCHEFRECEITAS;
+
+-- Criação da tabela Paciente
+IF NOT EXISTS( SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'ProdutoCardapio')
+
+BEGIN
+	CREATE TABLE ProdutoCardapio (
+		id INT IDENTITY(1,1) PRIMARY KEY,
+		nomeCardapio VARCHAR(100) NOT NULL,
+		descricaoCardapio VARCHAR(200) NOT NULL,
+		ingredientesCardapio VARCHAR(300) NOT NULL,
+		preparoCardapio VARCHAR(1000) NOT NULL,
+		observacaoCardapio VARCHAR(200),
+		pesoFinal DECIMAL NOT NULL
+	);
+END
